9db1e5e3369d66efc7d2e061ef69500c083c9809
Damilola Omoyiwola <damomos123@gmail.com>
2021-12-17 22:35:57 +0100

    Add progress bar to depict loading state

--- app/src/main/java/com/test/randomusers/ui/users/UserListFragment.kt
+++ app/src/main/java/com/test/randomusers/ui/users/UserListFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.test.randomusers.R
 import com.test.randomusers.data.model.User
@@ -71,7 +73,7 @@ class UserListFragment : Fragment() {
 
     private fun onUserClicked(user: User) {
         userListSaveState = binding.userRecyclerview.layoutManager?.onSaveInstanceState()
-        //indNavController().navigate(UserListFragmentDirections.toUserDetailsFragment(user))
+        findNavController().navigate(UserListFragmentDirections.toUserDetailsFragment(user))
     }
 
     private fun showNoInternet() {
@@ -89,10 +91,10 @@ class UserListFragment : Fragment() {
     }
 
     private fun showLoading() {
-
+        binding.progressBar.isVisible = true
     }
 
     private fun hideLoading() {
-
+        binding.progressBar.isVisible = false
     }
 }
